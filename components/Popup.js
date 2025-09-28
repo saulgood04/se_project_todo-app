8304fcf939dc526ea69ed67f898694b840d28241
Saul Good <saulgood04@gmail.com>
2025-09-28 18:58:46 -0400

    added part 6

--- components/Popup.js
+++ components/Popup.js
@@ -4,19 +4,33 @@ class Popup {
     this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
   }
 
+  _handleEscapeClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
   open() {
     this._popupElement.classList.add("popup_visible");
+    document.addEventListener("keyup", this._handleEscapeClose.bind(this));
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
-    
+    document.removeEventListener("keyup", this._handleEscapeClose);
   }
 
   setEventListeners() {
-    this._popupCloseBtn.addEventListener("click", () => {
-  this.close();
-});
+    
+
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      
+      if (
+        evt.target.classList.contains("popup__close") ||
+        evt.target.classList.contains("popup")
+      )
+        this.close();
+    });
   }
 }
 
